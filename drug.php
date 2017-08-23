871da2925c1fa5f3a402c39a0e5f06a834e2e355
Skilfulsidiq <osodiq@gmail.com>
2017-08-23 15:39:04 +0100

    second

--- drug.php
+++ drug.php
@@ -1,26 +1,50 @@
 <?php
-  require_once 'config.php';
+  require_once 'config.php';?>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <!-- Required meta tags always come first -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+  	<!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  </head>
+  <body>
+    <div class="container">
+      <a href="index.php" class="btn btn-success" style="margin-top:10px; ">Search</a>
+  <?php
   if (isset($_GET['list'])) {
     $search = $_GET['list'];
     $sql = "SELECT * FROM drug WHERE id = '$search'";
     $result = mysqli_query($conn,$sql); ?>
-    <table class="table table-condesed table-striped">
-      <thead>
-        <th>Drug</th><th>Disease</th><th>Dosage</th><th>Adverse Effect</th><th>Manufacturer</th>
-      </thead>
+    <table class="text-center table table-condesed table-striped table-bordered">
+      <?php while ($row = mysqli_fetch_assoc($result)) :?>
+      <caption class="text-center" style="font-weight:bold;font-size:35px;"><?php echo $row['name']; ?> Details</caption>
       <tbody>
-      <?php  while ($row = mysqli_fetch_assoc($result)) : ?>
-        <tr>
-          <td><?php echo $row['name'];?></td>
-          <td><?php echo $row['disease'];?></td>
-          <td><?php echo $row['dosage'];?></td>
-          <td><?php echo $row['adverse_effect'];?></td>
-          <td><?php echo $row['manufacturer'];?></td>
+         <td>Disease it Cure </td>
+         <td><?php echo $row['disease'];?> </td>
         </tr>
-      <?php endwhile; ?>
-    </tbody>
-    </table>
+        <tr>
+          <td>Dosage </td>
+          <td><?php echo $row['dosage'];?> </td>
+         </tr>
+         <tr>
+           <td>Adverse Effect </td>
+           <td><?php echo $row['adverse_effect'];?> </td>
+          </tr>
+          <tr>
+            <td>Manufactured By </td>
+            <td><?php echo $row['manufacturer'];?> </td>
+           </tr>
 
-<?php  }
+</tbody>
+<?php endwhile; ?>
+</table>
+
+<?php }
 
 ?>
+</div>
+</body>
+</html>
